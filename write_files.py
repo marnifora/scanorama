7307a7f020d62d63c3fdb9254c587e2fd6124866
marnifora <marnifora@gmail.com>
2019-09-21 11:27:02 +0200

    Small corrections.

--- write_files.py
+++ write_files.py
@@ -1,8 +1,11 @@
-from bin.config import namespace, output, data_names
+from bin.config import namespace, output, data_names, names
 from bin.process import load_names
 from scipy.io import mmwrite
 from scipy.sparse import vstack
+from scanorama.scanorama import merge_datasets
 
-datasets, _, _, _ = load_names(data_names, norm=False)
+datasets, genes_list, cells_list, n_cells = load_names(data_names, norm=False)
+
+datasets, genes = merge_datasets(datasets, genes_list, ds_names=names, union=False)
 
 mmwrite(output + '%s_datasets.mtx' % namespace, vstack(datasets))
