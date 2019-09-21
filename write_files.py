2c33b27c033253aa5c36b76d959fc506bcc052a0
marnifora <marnifora@gmail.com>
2019-09-21 11:33:51 +0200

    Small corrections.

--- write_files.py
+++ write_files.py
@@ -8,4 +8,4 @@ datasets, genes_list, cells_list, n_cells = load_names(data_names, norm=False)
 
 datasets, genes = merge_datasets(datasets, genes_list, ds_names=names, union=False)
 
-mmwrite(output + '%s_datasets.mtx' % namespace, vstack(datasets))
+mmwrite(output + '%s_datasets.mtx' % namespace, vstack(datasets), field='integer')
