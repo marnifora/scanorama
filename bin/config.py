7f45d3b586a9042e4c3526dce1abf058afd634b8
marnifora <marnifora@gmail.com>
2019-11-26 15:36:47 +0100

    Improving establishing namespace.

--- bin/config.py
+++ bin/config.py
@@ -15,7 +15,7 @@ parser.add_argument('--batches', action='store', metavar='FILE', type=str, defau
 parser.add_argument('-o', '--output', action='store', metavar='OUT', type=str, required=False,
                     default=None, help='Directory for the results, if not given - it is [PATH]/results/')
 parser.add_argument('-n', '--namespace', action='store', metavar='NAMESPACE', type=str, required=False,
-                    default='panorama', help='Namespace for the result files')
+                    default=None, help='Namespace for the result files, if not given - the same as input file')
 parser.add_argument('-m', '--metadata', action='store', metavar='METADATA', type=str, required=False,
                     default=None, help='Optional file with metadata')
 parser.add_argument('-pc', '--dimred', action='store', metavar='NUMBER', type=int, required=False,
@@ -27,9 +27,18 @@ parser.add_argument('--scanpy_pca', action='store_true')
 parser.add_argument('--no_adjust', action='store_true')
 args = parser.parse_args()
 
-path, namespace, write, tsne, uncorrected, dimred, metadata, scanpy = \
-    args.path, args.namespace, args.write, args.tsne, args.uncorrected, args.dimred, args.metadata, \
+path, write, tsne, uncorrected, dimred, metadata, scanpy = \
+    args.path, args.write, args.tsne, args.uncorrected, args.dimred, args.metadata, \
     args.scanpy_pca
+
+if args.namespace is None:
+    if args.mtx is not None:
+        string = args.mtx.strip('.mtx')
+    elif args.conf is not None:
+        string = args.conf.strip('.txt')
+    namespace = string.split('/')[-1].split('_')[0]
+else:
+    namespace = args.namespace
 
 if args.no_adjust:
     adjust = False
@@ -93,7 +102,7 @@ else:
 if args.metadata is not None:
     metafile = args.metadata
 else:
-    metafile = '.'.join(args.file.split('.')[:-1]) + '_metadata.txt'
+    metafile = '{}_metadata.tsv'.format(namespace)
 if os.path.isfile(metafile):
     with open(metafile, 'r') as f:
         metadata = {}
